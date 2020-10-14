a962165d746a6a24de5e72492a74953a1b63ca01
forza111 <nikitos.ionkin@yandex.ru>
2020-10-14 09:26:16 +0300

    #3 (20 minutes)

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
+
+class Articles(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    #primary_key говорит что значение id должно бть уникальным
+    title = db.Column(db.String(100), nullable = False)
+    intro = db.Column(db.String(300), nullable = False)
+    text = db.Column(db.Text, nullable = False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __rapr__(self):
+        return '<Article {}>'.format(self.id)
 
 @app.route('/')
 @app.route('/home')
@@ -14,9 +30,16 @@ def about():
     return render_template('/about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name,id):
-    return 'User page: {} - {}'.format(name,id)
+@app.route('/create-article', metrhods = ['POST', 'GET'])
+def create_article():
+    if request.method = 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Articles(title=title, intro = intro, text=text)
+    else:
+        return render_template('create-article.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
