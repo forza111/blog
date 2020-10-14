a01a652109e63e4ec9ba08e22b86c0c833e00258
forza111 <nikitos.ionkin@yandex.ru>
2020-10-14 22:37:08 +0300

    #4

--- app.py
+++ app.py
@@ -30,6 +30,18 @@ def about():
     return render_template('/about.html')
 
 
+@app.route('/posts')
+def posts():
+    articles = Articles.query.order_by(Articles.date.desc()).all()#desc- от нового к старому
+    return render_template('/posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Articles.query.get(id)
+    return render_template('/post_detail.html', article=article)
+
+
 @app.route('/create-article', methods = ['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
@@ -42,7 +54,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect('/')
+            return redirect('/posts')
         except:
             return 'При добавлении страницы произошла ошибка'
 
